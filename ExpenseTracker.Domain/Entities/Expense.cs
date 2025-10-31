975c09a589caab7f881f48d7bff9b01a73d6de70
smazhserg@gmail.com <smazhserg@gmail.com>
2025-10-31 11:42:22 +0300

    feat(domain): add Expense entity

---
+++ ExpenseTracker.Domain/Entities/Expense.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Domain.Entities
+{
+    public class Expense
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; } = "EUR";
+        public string Category { get; set; } = "General";
+        public string? Note { get; set; }
+        public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+
+    }
+}
