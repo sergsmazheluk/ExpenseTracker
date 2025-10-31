c241ce7daff0267397fea92bbbb058e521cc16ec
smazhserg@gmail.com <smazhserg@gmail.com>
2025-10-31 11:47:48 +0300

    feat(application): define IAppDbContext

---
+++ ExpenseTracker.Application/Abstractions/IAppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using ExpenseTracker.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Application.Abstractions
+{
+    public interface IAppDbContext
+    {
+        DbSet<Expense> Expenses { get; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
--- ExpenseTracker.Domain/Entities/Expense.cs
+++ ExpenseTracker.Domain/Entities/Expense.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ExpenseTracker.Domain.Entities
+﻿namespace ExpenseTracker.Domain.Entities
 {
     public class Expense
     {
